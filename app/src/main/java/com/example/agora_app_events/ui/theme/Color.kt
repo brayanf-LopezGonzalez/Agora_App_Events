af19036e3377b83989493c5e9656146d2850802c
brayanf-LopezGonzalez <brayanflg99@gmail.com>
2026-06-03 12:26:25 -0700

    Thema, colors, typography, and some icons

--- app/src/main/java/com/example/agora_app_events/ui/theme/Color.kt
+++ app/src/main/java/com/example/agora_app_events/ui/theme/Color.kt
@@ -2,10 +2,22 @@ package com.example.agora_app_events.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val AgoraDark = Color(0xFF2D3142)
+val AgoraDarker = Color(0xFF1A1E2E)
+val AgoraSurface = Color(0xFF232738)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val AgoraOrange = Color(0xFFE87E2B)
+val AgoraOrangeLight = Color(0xFFF9A05C)
+
+val StatusActive = Color(0xFF4AC869)
+val StatusOngoing = Color(0xFFE87E2B)
+val StatusFull = Color(0xFF848484)
+val StatusFinished = Color(0xFF5E86F3)
+val StatusCanceled = Color(0xFFFF5B5B)
+
+val BtnConfirm = Color(0xFF08AB31)
+val BtnDanger = Color(0xFFE24B4A)
+
+val TextPrimary = Color(0xFF000000)
+val TextSecondary = Color(0xFF4A4A4A)
+val TextHint = Color(0xFF9E9E9E)
